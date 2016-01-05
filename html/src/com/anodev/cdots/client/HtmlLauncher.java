0795609e4c12b2ac7ea77cf767594b2fef1acabf
Abdullah <abonaser.87@gmail.com>
2016-01-06 01:15:38 +0300

    Setup LibGdx

---
+++ html/src/com/anodev/cdots/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.anodev.cdots.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.anodev.cdots.DotsGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new DotsGame();
+        }
+}
